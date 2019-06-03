1c3680ffc6588426263c49787625bdc45c1b8a9d
Alex <ascottjacobsen@gmail.com>
2019-06-03 21:33:26 +0100

    removing fade out from top of servOps div since it is hiding txt too early

--- assets/js/serving-js/serving.js
+++ assets/js/serving-js/serving.js
@@ -56,28 +56,28 @@ function buildCountryContainer(container, country, countryData) {
     countryInfo.classList.add('country-info', 'hidden-div')
     countryInfo.setAttribute('id', 'current-country-info')
 
-    countryInfo.addEventListener('scroll', function(){
-        let pageTop = countryInfo.offsetTop
-        let pageBottom = pageTop + window.innerHeight
-        let tagsList = document.getElementById('opportunities-grid').querySelectorAll('*')
+    // countryInfo.addEventListener('scroll', function(){
+    //     let pageTop = countryInfo.offsetTop
+    //     let pageBottom = pageTop + window.innerHeight
+    //     let tagsList = document.getElementById('opportunities-grid').querySelectorAll('*')
         
-        for (let i = 0; i < tagsList.length; i++) {
-            let tag = tagsList[i]
-            let tagChildren = tag.children
+    //     for (let i = 0; i < tagsList.length; i++) {
+    //         let tag = tagsList[i]
+    //         let tagChildren = tag.children
 
-            for (let x = 0; x < tagChildren.length; x++) {
-                let thisTag = tagChildren[x]
-                let rect = thisTag.getBoundingClientRect()
+    //         for (let x = 0; x < tagChildren.length; x++) {
+    //             let thisTag = tagChildren[x]
+    //             let rect = thisTag.getBoundingClientRect()
                 
-                if (rect.top < (pageTop - 50)) {
-                    thisTag.classList.add('fade-out')
+    //             if (rect.top < (pageTop - 50)) {
+    //                 thisTag.classList.add('fade-out')
                     
-                } else {
-                    thisTag.classList.remove('fade-out')
-                }
-            }
-        }
-    })
+    //             } else {
+    //                 thisTag.classList.remove('fade-out')
+    //             }
+    //         }
+    //     }
+    // })
 
     let closeContainerButton = document.createElement('span')
     let closeContainerDiv = document.createElement('div')
