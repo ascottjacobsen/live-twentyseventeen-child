8f914f33d22500d80596cb926137e50b6a845860
Alex <ascottjacobsen@gmail.com>
2020-03-03 11:43:10 +0000

    fix space on home header, fix contact url params - the main gem logo on the home page and text to the right was being positioned too low because of margins - added title casing to the subject line URL parameters in contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,20 +1,24 @@
 var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split("&"),
+    sParameterName,
+    i;
 
-        console.log(sURLVariables);
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split("=");
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
     }
+  }
 };
 
-var subject = getUrlParameter('subject');
+toTitleCase = str => {
+  return str
+    .split(/-/g)
+    .map(word => word[0].toUpperCase() + word.substr(1).toLowerCase())
+    .join(" ");
+};
 
-document.getElementById('field_e6lis62').value = subject
-console.log('hello')
+var subject = toTitleCase(getUrlParameter("subject"));
+
+document.getElementById("field_e6lis62").value = subject;
